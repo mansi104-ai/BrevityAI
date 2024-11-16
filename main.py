1e5cf20ef35b8506aa5390c9ac9b4c3b4dea59d4
mansi104-ai <118623172+mansi104-ai@users.noreply.github.com>
2024-11-16 11:27:05 +0530

    readme updated

--- main.py
+++ main.py
@@ -107,14 +107,17 @@ def create_pdf_in_memory(summary_text):
 
 # App Interface
 st.markdown(
+    
     """
     <div style="text-align: center;">
-        <img src="/logo_2.png" alt="BrevityAI Logo" style="height: 100px; margin-bottom: 20px;">
+        <img src="logo_2.png" alt="BrevityAI Logo" style="height: 100px; margin-bottom: 20px;">
     </div>
     <h1 class="title">📄 Text Summarization App</h1>
     """,
     unsafe_allow_html=True,
 )
+
+
 
 st.write("Upload a PDF/JSON file or enter text to summarize.")
 
